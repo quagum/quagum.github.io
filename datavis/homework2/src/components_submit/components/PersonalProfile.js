05ab9757343db3773315ccd393ae5bf1ec774d0a
Charles Wang <quagum125@gmail.com>
2025-02-05 16:19:02 -0500

    cleanup

--- datavis/homework2/src/components_submit/components/PersonalProfile.js
+++
@@ -1,8 +0,0 @@
-export default function PersonalProfile({profile}){
-    return(
-        <section class="personal-profile">
-            <h2 class="section-header">{profile.title}</h2>
-            <p>{profile.content}</p>
-        </section>
-    )
-}
