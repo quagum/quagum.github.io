812c712e0aea1cbba61fca8552284f46f7b37d72
Charles Wang <quagum125@gmail.com>
2025-02-21 18:42:15 -0500

    w

--- datavis/homework3/next.config.mjs
+++ datavis/homework3/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/datavis/homework3", // Match your repo path
-  assetPrefix: "/datavis/homework3/",
-  output: "export"
+  output: 'export', // Enables static export
+  basePath: '/datavis/homework3/prod', // Sets the base path for your app
+  assetPrefix: '/datavis/homework3/prod/', // Ensures assets are loaded correctly
 };
 
 export default nextConfig;
