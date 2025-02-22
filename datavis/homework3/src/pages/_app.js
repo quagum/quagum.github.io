e68a56f9f6621e4514fecf2d27d4c4d8bb017b5a
Charles Wang <quagum125@gmail.com>
2025-02-21 19:12:58 -0500

    w

--- datavis/homework3/src/pages/_app.js
+++ datavis/homework3/src/pages/_app.js
@@ -1,4 +1,6 @@
-import "../styles/globals.css"; 
-export default function App() {
-  return <div></div>;
+// src/pages/_app.js
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
